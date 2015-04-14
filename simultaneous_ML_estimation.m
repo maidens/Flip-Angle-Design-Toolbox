f1e955913b2e16b5222995aaf3b396a5102eadcb
John Maidens <maidens@eecs.berkeley.edu>
2015-04-14 16:31:25 -0700

    Allow an initial starting point to be supplied for simultaneous maximum likelihood estimation

--- simultaneous_ML_estimation.m
+++ simultaneous_ML_estimation.m
@@ -1,26 +1,23 @@
 function [parameters_of_interest_est, nuisance_parameters_est] ...
-    = simultaneous_ML_estimation(model, y1, y2, thetas1, thetas2)
-    % FUNCTION PARAMETER_ESTIMATION computes an estimate of model parameters from data y
-    %   Choice of goodness_of_fit_criterion: 
-    %      * 'least-squares'
-    %      * 'maximum-likelihood'
+    = simultaneous_ML_estimation(model, y1, y2, thetas1, thetas2, p_init)
+
     %
     % Flip Angle Design Toolbox 
     % John Maidens (maidens@eecs.berkeley.edu) 
     % June 2014 
     
-    display('===== Estimating parameter values =====') 
-   
+
+        
     % perform maximum-likelihood fit 
-    options = optimset('MaxFunEvals', 5000, 'MaxIter', 5000); 
+    options = optimset('Display','iter','MaxFunEvals', 1000, 'MaxIter', 5000); 
     obj = @(p) negative_log_likelihood_rician(p, y1, ...
-        thetas1*model.flip_angle_input_matrix', model) ...
+        thetas1, model) ...
      + negative_log_likelihood_rician(p, y2, ...
-        thetas2*model.flip_angle_input_matrix', model); 
+        thetas2, model); 
 
     % initial point 
-    p_init =  [model.parameters_of_interest_nominal_values, ...
-        model.nuisance_parameters_nominal_values]; 
+    %p_init =  [model.parameters_of_interest_nominal_values, ...
+    %    model.nuisance_parameters_nominal_values] 
 
     p_opt = fminunc(obj, p_init, options);
 
